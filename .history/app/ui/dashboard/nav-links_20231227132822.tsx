af4b5a84e95bb7635b44e4ebccc613db17e7ee9a
Corey Burns <coreyburns@outlook.com>
2023-12-27 13:33:12 -0500

    first commit

--- .history/app/ui/dashboard/nav-links_20231227132822.tsx
+++ .history/app/ui/dashboard/nav-links_20231227132822.tsx
@@ -5,7 +5,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
-// Map of links to display in the side navigation.`
+// Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },

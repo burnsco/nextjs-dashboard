7c30ed4f4ace748f422bbb43e3592c096ff4523c
Corey Burns <coreyburns@outlook.com>
2024-09-14 17:00:49 -0400

    added try/catch blocks to all my server actions, with error messages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,42 +14,57 @@ const FormSchema = z.object({
 });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-const DeleteInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-	const { customerId, amount, status } = CreateInvoice.parse({
-		customerId: formData.get('customerId'),
-		amount: formData.get('amount'),
-		status: formData.get('status'),
-	});
-	const amountInCents = amount * 100;
-	const date = new Date().toISOString().split('T')[0]
+	try {
+		const { customerId, amount, status } = CreateInvoice.parse({
+			customerId: formData.get('customerId'),
+			amount: formData.get('amount'),
+			status: formData.get('status'),
+		});
+		const amountInCents = amount * 100;
+		const date = new Date().toISOString().split('T')[0]
 
-	await sql`
-		INSERT INTO invoices (customer_id, amount, status, date)
-		VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-	`;
-	revalidatePath('/dashboard/invoices')
-	redirect('/dashboard/invoices')
+		await sql`
+			INSERT INTO invoices (customer_id, amount, status, date)
+			VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+		`;
+		revalidatePath('/dashboard/invoices')
+		redirect('/dashboard/invoices')
+
+	} catch (error) {
+		return {
+			message: "Database Error: Failed to Create Invoice."
+		}
+	}
+
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-	const { customerId, amount, status } = UpdateInvoice.parse({
-		customerId: formData.get('customerId'),
-		amount: formData.get('amount'),
-		status: formData.get('status'),
-	});
+	try {
+		const { customerId, amount, status } = UpdateInvoice.parse({
+			customerId: formData.get('customerId'),
+			amount: formData.get('amount'),
+			status: formData.get('status'),
+		});
 
-	const amountInCents = amount * 100;
+		const amountInCents = amount * 100;
 
-	await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+		await sql`
+			UPDATE invoices
+			SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+			WHERE id = ${id}
+		`;
 
-	revalidatePath('/dashboard/invoices');
-	redirect('/dashboard/invoices');
+		revalidatePath('/dashboard/invoices');
+		redirect('/dashboard/invoices');
+
+	} catch (error) {
+		return {
+			message: "Database Error: Failed to Update Invoice."
+		}
+	}
+
 }
 
 export async function deleteInvoice(id: string) {
